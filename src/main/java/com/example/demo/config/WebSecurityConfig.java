e018265b58ead1198e2fd2259ff5e38a9a11dc9c
Funya1337 <funya7654@gmail.com>
2023-10-19 00:04:44 +0300

    feat(auth): no encrypt login and reg, transact sql

---
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,86 @@
+package com.example.demo.config;
+
+import javax.sql.DataSource;
+
+import org.apache.catalina.authenticator.SpnegoAuthenticator.AuthenticateAction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import com.example.demo.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Autowired
+	private UserService userService;
+
+	// @Bean
+	// public BCryptPasswordEncoder bCryptPasswordEncoder() {
+	// 	return new BCryptPasswordEncoder();
+	// }
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+		auth.setUserDetailsService(userService);
+		auth.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		return auth;
+	}
+
+	// @Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+		MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+		http.authorizeHttpRequests((requests) -> requests
+			.requestMatchers(mvcMatcherBuilder.pattern("/registration")).permitAll()
+			.anyRequest().authenticated()
+		)
+		.formLogin((form) -> form
+			.loginPage("/login")
+			.permitAll()
+		)
+		.logout((logout) -> logout.permitAll());
+
+	return http.build();
+	}
+
+	// @Bean
+	// public UserDetailsService userDetailsService() {
+	// 	UserDetails user =
+	// 		 User.withDefaultPasswordEncoder()
+	// 			.username("user")
+	// 			.password("password")
+	// 			.roles("USER")
+	// 			.build();
+
+	// 	return new InMemoryUserDetailsManager(user);
+	// }
+
+	// protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	// 	auth.jdbcAuthentication()
+	// 	.dataSource(dataSource)
+	// 	.passwordEncoder(NoOpPasswordEncoder.getInstance())
+	// 	.usersByUsernameQuery("select email, password from users where email=?")
+	// 	.authoritiesByUsernameQuery("select u.email, ur.roles from users u inner join users_roles ur on u.id = ur.id where u.email=?");
+	// }
+	
+}
